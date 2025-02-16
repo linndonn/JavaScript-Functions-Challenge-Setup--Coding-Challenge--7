7636c55dc80e963443022f10ab47bede99f41b34
linndonn <linndonn@usf.edu>
2025-02-16 06:47:47 -0500

    Task 3 - Customer Loyalty Discount.

--- cc_7.js
+++ cc_7.js
@@ -18,3 +18,22 @@ const calculateHourlyWage = function(salary, hoursPerWeek) {
 // Test Data:
 calculateHourlyWage(52000, 40); // Expected output: "Hourly Wage: $25.00"
 calculateHourlyWage(75000, 35); // Expected output: "Hourly Wage: $41.21"
+
+//Task 3 - Customer Loyalty Discount
+//Write an arrow function calculateLoyaltyDiscount(amount, years) that applies a discount:
+const calculateLoyaltyDiscount = (amount, years) => {
+//years >= 5: 15% discount
+let LoyaltyDiscount;
+if (years >= 5) LoyaltyDiscount = 0.15;
+//years >= 3: 10% discount
+else if (years >= 3) LoyaltyDiscount = 0.1;
+//years < 3: 5% discount
+else LoyaltyDiscount = 0.05;
+//Formula to calculate the price after discount
+const discountedPrice = amount * (1 - LoyaltyDiscount);
+//Logged into console
+console.log(`Discounted price: $${discountedPrice.toFixed(2)}`);
+};
+//Test Data:
+calculateLoyaltyDiscount(100, 6); // Expected output: "Discounted Price: $85.00"
+calculateLoyaltyDiscount(200, 2); // Expected output: "Discounted Price: $190.00"
