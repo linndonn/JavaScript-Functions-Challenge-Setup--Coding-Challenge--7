273885e3790bde3681e878008bb6e17a3f18a09a
linndonn <linndonn@usf.edu>
2025-02-16 07:12:58 -0500

    Task 4 - Product Shipping Cost Calculation

--- cc_7.js
+++ cc_7.js
@@ -37,3 +37,23 @@ console.log(`Discounted price: $${discountedPrice.toFixed(2)}`);
 //Test Data:
 calculateLoyaltyDiscount(100, 6); // Expected output: "Discounted Price: $85.00"
 calculateLoyaltyDiscount(200, 2); // Expected output: "Discounted Price: $190.00"
+
+// Task 4 - Product Shipping Cost Calculation
+// Write a function calculateShippingCost(weight, location, expedited = false) to calculate shipping fees:
+function calculateShippingCost(weight, location, expedited = false) {
+    let cost = 0;
+    // USA shipping fee = $5.00 + $0.50 per pound:
+    if (location === "USA") {
+      cost = 5 + (0.50 * weight);} 
+    // Canada shipping fee = $10.00 + $0.70 per pound:
+    else if (location === 'Canada') {
+      cost = 10 + (0.7 * weight);}
+   // Expedited shipping = additional $10.00
+    if (expedited) {
+      cost += 10.00;}
+ // Logging into console
+    console.log(`Shipping Cost: $${cost.toFixed(2)}`);
+  };
+  //Test Data:
+  calculateShippingCost(10, "USA", true); // Expected output: "Shipping Cost: $20.00"
+calculateShippingCost(5, "Canada", false); // Expected output: "Shipping Cost: $13.50"
